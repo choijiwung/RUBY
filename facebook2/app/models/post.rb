aa915b714d44481b71fa48ce6d7032d8e1e2120d
choijiwung <784wldnd@naver.com>
2017-12-05 17:41:03 +0900

    20171205

--- facebook2/app/models/post.rb
+++
@@ -1,8 +0,0 @@
-class Post < ApplicationRecord
-  belongs_to :user
-  has_many :comments, dependent: :destroy
-  # 주인없는 친구들을 날려주는
-  validates :title, presense: true
-  validates :content, presense: true
-  
-end
